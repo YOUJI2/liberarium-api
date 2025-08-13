d60d786e7ceb187064ccfac3227cc3c139ba054d
jihoonyou <dbwlgna98@naver.com>
2025-08-13 15:29:18 +0900

    [#1]chore: 프로젝트 초기 셋팅

---
+++ src/main/java/com/liberarium/api/LiberariumApiApplication.java
@@ -0,0 +1,11 @@
+package com.liberarium.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberariumApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiberariumApiApplication.class, args);
+    }
+}
