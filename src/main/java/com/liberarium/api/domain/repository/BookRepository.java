e8bb51dc072d2351c20b0f7f591d5089adf013d3
jihoonyou <dbwlgna98@naver.com>
2025-08-18 01:55:29 +0900

    [#5]feat: 도서 상세 조회 API 서비스 구현 (멀티인스턴스 기준 동시성 제어 구현)

--- src/main/java/com/liberarium/api/domain/repository/BookRepository.java
+++ src/main/java/com/liberarium/api/domain/repository/BookRepository.java
@@ -1,9 +1,20 @@
 package com.liberarium.api.domain.repository;
 
 import com.liberarium.api.domain.entity.Book;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, String> {
 
-
+  @Query("""
+    select distinct b
+    from Book b
+    left join fetch b.detail d
+    left join fetch b.catalogs cts
+    left join fetch cts.catalog c
+    where b.isbn = :isbn
+  """)
+  Optional<Book> findBookDetailInfoByIsbn(@Param("isbn") String isbn);
 }
